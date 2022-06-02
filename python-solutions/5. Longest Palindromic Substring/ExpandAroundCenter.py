ef796b71f425fbfd25eb681876403d1eeb7f3890
kothakon <106774657+kothakon@users.noreply.github.com>
2022-06-02 17:49:21 -0400

    Update ExpandAroundCenter.py

--- python-solutions/5. Longest Palindromic Substring/ExpandAroundCenter.py
+++ python-solutions/5. Longest Palindromic Substring/ExpandAroundCenter.py
@@ -1,1 +1,8 @@
+# Runtime Complexity: O(N^2)
+# Spacetime Complexity: O(1)
+
+# Intuition: Every palindrome has a center of length one or two. 
+# 1) Iterate through string and expand around each center.
+# 2) If valid palindrome is created, update max length.
+# 3) return max length
 
