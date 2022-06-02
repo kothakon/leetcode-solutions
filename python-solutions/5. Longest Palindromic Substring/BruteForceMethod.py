b5216dd02b6a3b2023ec8d8c074166cdef6b81a1
kothakon <106774657+kothakon@users.noreply.github.com>
2022-06-02 17:37:26 -0400

    Update BruteForceMethod.py

--- python-solutions/5. Longest Palindromic Substring/BruteForceMethod.py
+++ python-solutions/5. Longest Palindromic Substring/BruteForceMethod.py
@@ -1,1 +1,34 @@
+# Runtime Complexity: O(N^3)
+# Spacetime Complexity: O(1)
+# A substring can be defined by a start index and an end index.
+# Can create every pair of start and end indices creates, essentially creating every substring
+# Also must validate each substring in linear time, hence N(start index)*N(end index)*N(validation) = O(N^3)
+# Results in TLE
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        # helper function to check if a string is palindrome
+        # takes left and right indices as boundaries to check
+        # return tuple of start and end indices and length of substring
+        def isPal(l: int, r: int) -> tuple:
+            start = l
+            end = r
+            while l <= r:
+                if s[l] != s[r]:
+                    return ((start,end),(0))
+                l += 1
+                r -= 1
+            return ((start,end), (end - start + 1))
+        
+        maxLength = length = start = end = 0
+        # create each substring with nested loops
+        for i in range(len(s)):
+            for j in range(i+1, len(s)):
+                bounds, length = isPal(i,j)
+                left, right = bounds
+                # update maxlength and bounds
+                if length > maxLength:
+                    start, end = left, right
+                    maxLength = length
+        return s[start:end+1]
