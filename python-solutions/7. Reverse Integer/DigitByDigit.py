5d3bfcdba44975de5488a38b3bb1649e579e3257
kothakon <106774657+kothakon@users.noreply.github.com>
2022-06-02 18:40:22 -0400

    Update DigitByDigit.py

--- python-solutions/7. Reverse Integer/DigitByDigit.py
+++ python-solutions/7. Reverse Integer/DigitByDigit.py
@@ -1,1 +1,5 @@
+# Time Complexity: O(N) where N is length of input string
+# Space Complexity: O(1) constant space
+
+# Iterate through array and use simple math to reverse integer digit by digit:
 
