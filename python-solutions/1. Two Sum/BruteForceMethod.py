25bf4c05d23e7fcfd43fab0d8e61fc135643f0aa
kothakon <106774657+kothakon@users.noreply.github.com>
2022-06-02 16:03:13 -0400

    Update BruteForceMethod.py

--- python-solutions/1. Two Sum/BruteForceMethod.py
+++ python-solutions/1. Two Sum/BruteForceMethod.py
@@ -1,1 +1,10 @@
-# 
+# Runtime Complexity: O(N^2)
+# Spacetime Complexity: O(1)
+# Use nested for loops to compare every pairing of elements
+# This method will result in TLE in other languages
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
