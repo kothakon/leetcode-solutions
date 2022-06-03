10c21d78b526b83ea64e4e54e5520a0e125efdde
kothakon <106774657+kothakon@users.noreply.github.com>
2022-06-03 14:35:20 -0400

    Update BruteForce.py

--- python-solutions/11. Container With Most Water/BruteForce.py
+++ python-solutions/11. Container With Most Water/BruteForce.py
@@ -1,1 +1,14 @@
+# Time Complexity: O(N^2)
+# Space Complexity: O(1) Constant space
+# Brute Force method. Results in TLE
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # Use two for loops to mark start and end of rectangle
+        # Essentially check every valid rectangle
+        maxA = area = 0
+        for i in range(len(height)):
+            for j in range(i+1,len(height)):
+                area = (j-i)*min(height[j],height[i])
+                maxA = max(maxA,area)
+        return maxA
